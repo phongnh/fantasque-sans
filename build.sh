c404c0303259b447a28324097d0b77b99fecd951
Phong Nguyen <nhphong1406@gmail.com>
2022-03-25 13:48:51 +0700

    Update build.sh and copy.sh for macOS

--- build.sh
+++ build.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env sh
 
 sudo make clean
+mkdir -p Variants
 docker build -t fantasque .
-docker run -v "$(pwd)/Variants:/fantasque/Variants" fantasque
+docker run --rm -v "$(pwd)/Variants:/fantasque/Variants" fantasque
