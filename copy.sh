9ec3023d6047385114698cc2ed2a44942283df7e
Phong Nguyen <nhphong1406@gmail.com>
2022-03-25 13:48:51 +0700

    Update build.sh and copy.sh for macOS

--- copy.sh
+++ copy.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env sh
 
 sudo cp ./Variants/FantasqueSansMono-*.zip ~/src/fonts/
-sudo chown -R $(whoami):$(whoami) ~/src/fonts/FantasqueSansMono-*.zip
+sudo chown -R $(whoami) ~/src/fonts/FantasqueSansMono-*.zip
